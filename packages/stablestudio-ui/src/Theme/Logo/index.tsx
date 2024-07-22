a43360730f57db836907d8f575d85c545e6dd79a
P-How <peihaoli1014@gmail.com>
2024-07-22 22:49:12 +0200

    updated logo

--- packages/stablestudio-ui/src/Theme/Logo/index.tsx
+++ packages/stablestudio-ui/src/Theme/Logo/index.tsx
@@ -1,7 +1,14 @@
 import { Next } from "./Next";
 
 export function Logo(props: JSX.IntrinsicElements["img"]) {
-  return <img src="/logo.png" alt="Logo" {...props} />;
+  return (
+    <img
+      src="/logo.png"
+      alt="Logo"
+      style={{ maxWidth: "30px", maxHeight: "30px" }}
+      {...props}
+    />
+  );
 }
 
 Logo.Next = Next;
