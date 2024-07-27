2d9a170c5dc0c804aacac0ea0dd00dd242d9e43e
Administrator <peihaoli1014@gmail.com>
2024-07-27 20:31:51 +0200

    updated headers

--- packages/stablestudio-plugin-webui/src/index.ts
+++ packages/stablestudio-plugin-webui/src/index.ts
@@ -260,7 +260,7 @@ export const createPlugin = StableStudio.createPlugin<{
   let webuiHostUrl = localStorage.getItem("webui-host-url");
 
   if (!webuiHostUrl || webuiHostUrl === "") {
-    webuiHostUrl = "https://f9e6-83-108-65-42.ngrok-free.app";
+    webuiHostUrl = "https://82cb-46-19-22-26.ngrok-free.app";
   }
 
   return {
@@ -363,12 +363,12 @@ export const createPlugin = StableStudio.createPlugin<{
       baseUrl: {
         type: "string",
         title: "Host URL",
-        placeholder: "https://f9e6-83-108-65-42.ngrok-free.app",
+        placeholder: "https://82cb-46-19-22-26.ngrok-free.app",
         value:
           localStorage.getItem("webui-host-url") ??
-          "https://f9e6-83-108-65-42.ngrok-free.app",
+          "https://82cb-46-19-22-26.ngrok-free.app",
         description:
-          "The URL of the `stable-diffusion-webui` host, usually https://f9e6-83-108-65-42.ngrok-free.app",
+          "The URL of the `stable-diffusion-webui` host, usually https://82cb-46-19-22-26.ngrok-free.app",
       },
 
       upscaler: {
